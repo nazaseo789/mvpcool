f9436d4cd6e31e64b34c61b709fb043cfade9497
nazaseo789 <nazaseo789@gmail.com>
2022-12-03 00:46:48 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,56 @@
+import path from 'path'
+import glob from 'glob'
+import head from './config/head'
+import { modules, modulesSettings } from './config/modules'
+import plugins from './config/plugins'
+import build from './config/build'
+import css from './config/css'
+import { routeMap, otherRoutes } from './config/generate'
+
 export default {
-  head: {
-    title: 'mvpcool',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'my website description'
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  }
+  target: 'static',
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Headers of the page
+   */
+  head: head,
+  generate: {
+    routes: otherRoutes.concat(getDynamicPaths(routeMap))
+  },
+  /*
+   ** Global CSS
+   */
+  css: css,
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: plugins,
+  /*
+   ** Nuxt.js modules
+   */
+  modules: modules,
+  ...modulesSettings,
+  /*
+   ** Build configuration
+   */
+  build: build
+}
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable
+ */
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map((url) => {
+      const filepathGlob = urlFilepathTable[url]
+      return glob.sync(filepathGlob, { cwd: 'content' }).map((filepath) => {
+        return `${url}/${path.basename(filepath, '.md')}`
+      })
+    })
+  )
 }
