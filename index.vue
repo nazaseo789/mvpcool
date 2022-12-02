c6ede6a8de3b7cdb181f3aca1d0537035b4a5906
nazaseo789 <nazaseo789@gmail.com>
2022-12-03 00:47:41 +0700

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  head: {
+    title: 'Home page',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description'
+      }
+    ],
+  }
+}
+</script>
